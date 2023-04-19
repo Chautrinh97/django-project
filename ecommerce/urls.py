62494374060ce975c1d9bc2eea3a8bda2066f41e
dochautrinh <102200395@sv1.dut.udn.vn>
2023-04-19 20:49:05 +0700

    test product view

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-
+    path('', views.Home.as_view(), name = 'home' ),
+    path('product/', views.SingleProduct.as_view(), name = 'single-product')
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+
+"""
+Product management
+"""
+class Home(View):
+    def get(self, request):
+        return render(request, 'product/index.html', {})
+    def post(self):
+        pass
+
+class SingleProduct(View):
+    def get(self, request):
+        return render(request, 'product/single-product.html', {})
+    def post(self,request):
+        pass
+
