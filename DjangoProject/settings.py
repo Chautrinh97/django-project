17191a8f3c062d89e4593489cfb8f130f748cc3d
dochautrinh <102200395@sv1.dut.udn.vn>
2023-05-18 18:05:28 +0700

    cm

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u$gx)odfn4yv%o&$%g1_s*7_p4rfhl0@81zui9a9wmou3ohvmr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
